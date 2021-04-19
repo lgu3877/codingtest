298396786337909b88153a898663fb5de9c771f6
gun uck <lgu3877@naver.com>
2021-04-20 00:57:57 +0900

    20210420 00:57 2번 문제 일치하는 문자 개수 찾기 오류 수정

--- codingtest/src/codingtest/exem/Solution2.java
+++ codingtest/src/codingtest/exem/Solution2.java
@@ -9,21 +9,29 @@ class Resolve2 {
 		int answer = 0;
 		
 		for(String word : words) {
-			
-			for(int i =0; i < word.length(); i++) {
-				String alphabet = String.valueOf(word.charAt(i));
-				
-				if(!target.contains(alphabet)) 
-					break;
-				
-				else if(i == word.length()-1)
-					answer++;
-			}
+			System.out.println(word + "결과 값 : " + includeWords(target, word));
+			 answer = answer + includeWords(target, word);
 		}
-		
+			
 		return answer;
 		
 	}
+	
+	private int includeWords(String target, String word) {
+		for(int i = 0; i < word.length(); i++) {
+			String alphabet = String.valueOf(word.charAt(i));
+			
+			if(target.contains(alphabet)) 
+				target = target.replace(alphabet, "");
+			else if(!target.contains(alphabet)) 
+				return 0;
+			
+		}
+		
+		
+		return 1;
+	}
+	
 }
 
 public class Solution2 {
@@ -31,8 +39,8 @@ public class Solution2 {
 	public static void main(String[] args) {
 		Resolve2 resolve = new Resolve2();
 		
-		int result1 = resolve.solution("abc", new String[] {"a","b","c","ab","ac","bc","abc"});
-		int result2 = resolve.solution("cloud", new String[] {"card","cd","lol","doc"});
+		int result1 = resolve.solution("abc", new String[] {"a","b","cav","abb","ac","bc","abc"});
+		int result2 = resolve.solution("cloud", new String[] {"card","cduol","lol","doc"});
 		
 		System.out.printf("result1 결과  => [ %d ] \n", result1);
 		System.out.printf("result2 결과  => [ %d ] \n", result2);
--- codingtest/src/codingtest/exem/Solution5.java
+++ codingtest/src/codingtest/exem/Solution5.java
@@ -7,8 +7,14 @@ public class Solution5 {
 		// 250 ~ 320 , 발단위는 5단위
 		int numberOfCase = (320-250) / 5;
 		
+		
+		
 		// BTS 멤버 7명 
-		double result = (double) 7 / (double)numberOfCase * 100;
+		double result =  (double) 7 / (double)numberOfCase * 100;
+
+		
+		
+//		13/14 * 7/14
 		
 		System.out.printf("BTS 멤버들 중 발사이즈가 한명이라도 같은 확율 결과 => %.1f%%",result);
 	}
