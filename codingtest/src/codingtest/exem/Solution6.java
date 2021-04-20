0936bfee10f74f2175d6c8149a3c52a7fd22da08
gun uck <lgu3877@naver.com>
2021-04-20 20:34:17 +0900

    20210420 20:34 소켓을 활용한 Http Header 출력

--- codingtest/src/codingtest/exem/Solution6.java
+++ codingtest/src/codingtest/exem/Solution6.java
@@ -1,36 +1,36 @@
 package codingtest.exem;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.List;
-import java.util.Map;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 
 // 6. Http Header 출력
 class Resolve6 {
-	public void solution(String url) {
-		try {
-			 
-			URL obj = new URL(url);
-			URLConnection conn = obj.openConnection();
-
-			Map<String, List<String>> map = conn.getHeaderFields();
-			
-			for (Map.Entry<String, List<String>> entry : map.entrySet()) 
-				System.out.println((entry.getKey()== null ? "" : entry.getKey() + " : " ) + entry.getValue());
-			
-		} catch (Exception e) {
-			 e.printStackTrace();
-		}
-
+	public void solution(String url) throws UnknownHostException, IOException {
+		Socket s = new Socket(InetAddress.getByName(url), 80);
+		PrintWriter pw = new PrintWriter(s.getOutputStream());
+		pw.print("HEAD / HTTP/1.1\r\n");
+		pw.print("Host: " + url + "\r\n\r\n");
+		pw.flush();
+		BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		String t;
+		while((t = br.readLine()) != null) System.out.println(t);
+		br.close();
 	}
 }
 
 public class Solution6 {
 
-	public static void main(String[] args) throws IOException {
-		Resolve6 resolve = new Resolve6();
-		resolve.solution("http://www.ex-em.com/");
+	public static void main(String[] args) throws UnknownHostException, IOException  {
+		Resolve6 resolve6 = new Resolve6();
+		
+		resolve6.solution("www.ex-em.com");
+		
 	}
+	
 }
