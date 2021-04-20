a370277f8aaf378428a4ca806a6823f406de3c6a
gun uck <lgu3877@naver.com>
2021-04-20 23:52:36 +0900

    20210420 23:52 발 사이즈가 같은 확률 구하기 잘못된 해결 수정

--- codingtest/src/codingtest/exem/Solution5.java
+++ codingtest/src/codingtest/exem/Solution5.java
@@ -4,19 +4,19 @@ package codingtest.exem;
 public class Solution5 {
 
 	public static void main(String[] args) {
-		// 250 ~ 320 , 발단위는 5단위
-		int numberOfCase = (320-250) / 5;
 		
+		final int NUMBEROFCASE = (320-250) / 5;
 		
+		float result = (float)13 / NUMBEROFCASE;
 		
-		// BTS 멤버 7명 
-		double result =  (double) 7 / (double)numberOfCase * 100;
+		for(int i = 7; i < 13; i++) 
+			result *= ((float) i / NUMBEROFCASE);
+			
 
-		
-		
-//		13/14 * 7/14
+		result = (1 - result) * 100;
 		
 		System.out.printf("BTS 멤버들 중 발사이즈가 한명이라도 같은 확율 결과 => %.1f%%",result);
+		
 	}
 
 }
