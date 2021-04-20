b2f86e6b3231bc501e0e1527cc9ad5d11a8733e3
gun uck <lgu3877@naver.com>
2021-04-20 21:33:12 +0900

    20210420 21:32 7-1 분 출력 수정

--- codingtest/src/codingtest/exem/Solution7to1.java
+++ codingtest/src/codingtest/exem/Solution7to1.java
@@ -3,19 +3,13 @@ package codingtest.exem;
 class Resolve7to1 {
 	public boolean solution() {
 		
-		//시간당 들어오는 물류 개수
 		final int NEWPRODUCTS = 2000;
 		
-		// 슬롯 개수
 		final int SLOTS = 6;
 		
-		// 최대 허용량 물건 개수
 		final int MAXPRODUCTS = 10000;
 		
 		
-		// @슬롯 1개가 처리할 수 있는 양
-		// 사람 한 명당  70분에 처리할 수 있는 물류량 
-		// (25 * 6) 차 한대에 들어갈 수 있는 물류 개수   (300개)
 		final int PERSONCAN = (25 * 6) * 2;  
 		
 		// (25 * 2) + 20 처리되는데 걸리는  시간 (분 단위)  (70분)
@@ -23,16 +17,17 @@ class Resolve7to1 {
 		
 		
 		// 1분당 손실되는 물량
-		float lossItemsPerMinute = ((float)NEWPRODUCTS / 60) - ((float)PERSONCAN * SLOTS / 70 );
+		float lossItemsPerMinute = ((float)NEWPRODUCTS / 60) - ((float)PERSONCAN * SLOTS / ANHOUR);
 		
 		// 재시작되는데 소요되는 분
 		float lossMinutes = MAXPRODUCTS / lossItemsPerMinute;
 		
 		// 재시작되는데 소요되는 시간
 		int lossHours = (int)lossMinutes / 60;
+		int lossMinute = (int)((lossMinutes - (int)lossMinutes) * 0.6 * 100);
 		
 		System.out.println("7-1번. 아래 두 가지 질의에 대한 답을 구하는 프로그래밍을 작성하세요.");
-		System.out.printf("멈출 수 밖에 없는 물량이라면 그것은 운영을 시작한 후 언제가 될까? %d시간 이후에 \n", lossHours );
+		System.out.printf("멈출 수 밖에 없는 물량이라면 그것은 운영을 시작한 후 언제가 될까? %d시간 %d분 이후에 \n", lossHours, lossMinute);
 		
 		
 		
