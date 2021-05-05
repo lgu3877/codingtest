7f15947cac7e61e483bc4cb798d754ea8f59fe08
lgu3877 <lgu3877@naver.com>
2021-05-05 17:59:26 +0900

    20210505 wen 17:59 phonenumberlist commit

---
+++ codingtest/src/codingtest/hash/PhoneNumberList.java
@@ -0,0 +1,18 @@
+package codingtest.hash;
+
+class Solution2 {
+    public boolean solution(String [] phoneNumberList) {
+
+        return true;
+
+    }
+}
+
+public class PhoneNumberList {
+
+    public static void main(String[] args) {
+        Solution2 solution2 = new Solution2();
+        boolean result = solution2.solution(new String [] {"119", "97674223", "1195524421"});
+        System.out.println(result);
+    }
+}
